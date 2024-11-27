e6a0dbeda17add812eec36aa757410b6fbcacd98
Entwoane <antoine.musci@gmail.com>
2024-11-27 14:04:01 +0100

    Adding main header file. Defining structure and functions prototypes

--- main.h
+++ main.h
@@ -1,18 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 /**
- * struct spec - structure with all the format specifiers
- * @spec: format specifier
+ * struct format_specifier - format spec
+ * @specifier: spec
+ * @print_func: pointer
  */
-typedef struct spec
+typedef struct format_specifier
 {
-	char *spec;
-	void(*print)(char, ...);
-
-} spec_format;
+	char specifier;
+	int (*print_func)(va_list);
+} format_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_char(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_integer(va_list args);
 
 #endif
